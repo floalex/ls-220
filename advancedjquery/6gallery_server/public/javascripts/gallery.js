0544a34a27d125aedb4a55acde20e9d010934c6b
Flo <flo.liang@my.com>
2016-12-17 08:01:15 +0000

    Add photo gallery server setup part1

--- advancedjquery/6gallery_server/public/javascripts/gallery.js
+++ advancedjquery/6gallery_server/public/javascripts/gallery.js
@@ -8,15 +8,40 @@ $(function() {
     templates[$tmpl.attr("id")] = Handlebars.compile($tmpl.html());
   });
   
+  $("[data-type=partial]").each(function() {
+    var $partial = $(this);
+    Handlebars.registerPartial($partial.attr("id"), $partial.html());
+  });
+  
   $.ajax({
     url: "/photos",
     // Returns an array of photos data in JSON format
     success: function(json) {
       photos = json;
       // Render the photos template, and write it to the #slides div
-      $("#slides").html(templates.photos({ photos: photos }));
+      renderPhotos();
       // Render the photo_information template
-      $("section > header").html(templates.photo_information(photos[0]));
+      renderPhotoInformation(0);
+      // request the comments data for the first photo and render it on page load
+      getCommentsFor(photos[0].id);
     }
   });
+  
+  function renderPhotos() {
+    $("#slides").html(templates.photos({ photos: photos }));
+  }
+  
+  function renderPhotoInformation(idx) {
+    $("section > header").html(templates.photo_information(photos[idx]));
+  }
+  
+  function getCommentsFor(idx) {
+    $.ajax({
+      url: "/comments",
+      data: "photo_id=" + idx,
+      success: function(comment_json) {
+        $("#comments ul").html(templates.comments({ comments: comment_json }));
+      }
+    });
+  };
 });
